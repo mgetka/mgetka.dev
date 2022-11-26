459b04a3e8d25ca23acedc51bfb838b86a9d1e8b
mgetka <michal.getka@gmail.com>
2022-11-26 01:32:38 +0100

    add fancy hamburger menu component

---
+++ assets/js/navbar.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    var collapsible = $("#header-nav .navbar-collapse.collapse");
+    var hamburger = $("#header-nav .navbar-toggler");
+
+    collapsible.on("show.bs.collapse", function () {
+        hamburger.addClass("opened");
+    });
+
+    collapsible.on("hide.bs.collapse	", function () {
+        hamburger.removeClass("opened");
+    })
+
+});
