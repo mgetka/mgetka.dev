b143fb4901056a1937b6afc88b1d84d5d183faf0
mgetka <michal.getka@gmail.com>
2022-11-26 23:49:25 +0100

    landing page added

---
+++ assets/js/landing.js
@@ -0,0 +1,59 @@
+$(function () {
+
+    $("#landing").each(function () {
+        var container = $(this);
+        var roles_list = container.find("#roles-list");
+
+        if (roles_list.length) {
+            roles_list.hide();
+
+            var roles = $.map(roles_list.find("li"), function(role) {
+                return $(role).text();
+            });
+
+            var roles_display = $("<span>");
+            roles_list.after($("<p>")
+                .addClass("roles-display")
+                .addClass("mb-3")
+                .text("I'm\xa0")
+                .append(roles_display));
+
+            var i = 0;
+            var deleting = false;
+
+            function step() {
+                var full_text = roles[i];
+                var dt;
+
+                var text_length = roles_display.text().length;
+
+                if (deleting) {
+                    text_length--;
+                } else {
+                    text_length++;
+                }
+
+                roles_display.text(full_text.substring(0, text_length));
+
+                if (!deleting && text_length == full_text.length) {
+                    dt = 2000;
+                    deleting = true;
+                } else if (deleting && text_length == 0) {
+                    deleting = false;
+                    i = (i + 1) % roles.length;
+                    dt = 1000;
+                } else {
+                    dt = 100 - Math.random() * 50;
+                    if (deleting) { dt /= 4; }
+                }
+
+                setTimeout(step, dt);
+            };
+
+            step();
+
+        }
+
+    });
+
+});
---
+++ content/_index.md
@@ -0,0 +1,16 @@
+---
+roles:
+    - a software engineer.
+    - a backend developer.
+    - a frontend developer.
+    - a loving father.
+    - a sysadmin.
+    - an IoT engineer.
+    - a DevOps engineer.
+subtext: "Or at least I have scratched the surface of those subjects\xa0¯\\_(ツ)_/¯"
+social:
+    - class: fa-brands fa-linkedin
+      link: https://www.linkedin.com/in/mgetka/
+    - class: fa-brands fa-github
+      link: https://github.com/mgetka
+---
